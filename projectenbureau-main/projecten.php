6079a139a592dde35203ad139d908f512af41789
JessemvD <jessevandriel01@gmail.com>
2024-04-26 08:38:46 +0200

    Update projecten.php

--- projectenbureau-main/projecten.php
+++ projectenbureau-main/projecten.php
@@ -54,8 +54,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Projecten</title>
     <style>
+        body {
+            font-family: sans-serif;
+        }
         table {
             width: 100%;
             border-collapse: collapse;
